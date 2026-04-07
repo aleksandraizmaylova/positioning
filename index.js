ff6fb560ce8b536bc01018a9960462459933f43e
aleksandraizmaylova <aleksandraizmaylova111@gmail.com>
2026-04-07 20:14:07 +0500

    progress bar

--- index.js
+++ index.js
@@ -5,3 +5,14 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+const el = document.querySelector('.progress');
+let x = 0;
+
+const interval = setInterval(() => {
+    x += 10;
+    el.style.background = `linear-gradient(to right, red ${x}%, lightgray ${x}%)`;
+
+    if (x >= 100) {
+        clearInterval(interval); // Останавливаем, когда дошли до 100
+    }
+}, 1000);
