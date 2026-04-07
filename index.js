1a6cc40e4fe356b0d8b992c86aa5587582ab4751
Ivan Gusarov <gusarivan52@gmail.com>
2026-04-07 19:47:11 +0500

    add modal window

--- index.js
+++ index.js
@@ -5,3 +5,16 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const openBtn = document.getElementById('modal_btn');
+const closeBtn = document.getElementById('close_btn');
+const modal = document.getElementById('modal_window');
+
+openBtn.addEventListener('click', () => {
+    modal.style.display = 'flex';
+});
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
